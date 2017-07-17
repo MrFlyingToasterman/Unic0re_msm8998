67cca8abeb7fbdb7e8531b530d0f1755384087a5
Darius Musiolik <MrFlyingToasterman@yahoo.de>
2017-07-17 15:19:16 +0200

    Optimized auto build sh

--- auto-build.sh
+++ auto-build.sh
@@ -15,43 +15,36 @@ export ARCH=arm64
 export SUBARCH=arm64
 
 # Paths
-KERNEL_DIR="android/kernel/op5"
 REPACK_DIR="android/kernel/repack"
-ZIP_MOVE="android/kernel/out/op5"
+ZIP_MOVE="android/kernel/packed_zip"
 ZIMAGE_DIR="arch/arm64/boot"
 
-function make_kernel {
-	  git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
-		echo
-		make $DEFCONFIG
-		make $THREAD
- 	cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR/zImage
-}
-
-function make_zip {
-		echo $REPACK_DIR
-		cd $REPACK_DIR
-		git clone https://github.com/osm0sis/AnyKernel2/
-		zip -r9 "$VER"-"$VARIANT".zip *
-		mv "$VER"-"$VARIANT".zip $ZIP_MOVE
-		cd $KERNEL_DIR
-}
-
-
-DATE_START=$(date +"%s")
+mkdir android/kernel
+mkdir android/kernel/repack
+mkdir android/kernel/packed_zip
 
 echo "Unic0re Kernel Creation Script:"
+echo ""
 export CROSS_COMPILE=aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-echo
 
-make_kernel
-make_zip
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
 
-echo "-------------------"
-echo "Build Completed in:"
-echo "-------------------"
+make $DEFCONFIG
+make $THREAD
 
-DATE_END=$(date +"%s")
-DIFF=$(($DATE_END - $DATE_START))
-echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
-echo
+cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR/zImage
+
+cd $REPACK_DIR
+
+git clone https://github.com/osm0sis/AnyKernel2/
+
+cp ../../../arch/arm64/configs/anykernel ./anykernel.sh
+
+mv AnyKernel2/* ./
+rm -rf AnyKernel2/
+
+zip -r9 "$VER"-"$VARIANT".zip *
+
+mv "$VER"-"$VARIANT".zip ../../../$ZIP_MOVE
+
+echo "ready."
